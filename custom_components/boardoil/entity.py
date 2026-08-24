0bd98fe0e152a93a9151f2c0c12f11b0b05b9e05
Andrew Jackson <andrew@codechimp.org>
2026-08-24 16:44:41 +0100

    Remove visit link

--- custom_components/boardoil/entity.py
+++ custom_components/boardoil/entity.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from homeassistant.const import CONF_HOST
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -35,9 +34,5 @@ class BoardOilEntity(CoordinatorEntity[BoardOilDataUpdateCoordinator]):
             sw_version=(
                 f"{coordinator.config_entry.runtime_data.version} "
                 f"({coordinator.config_entry.runtime_data.build})"
-            ),
-            configuration_url=(
-                f"{str(coordinator.config_entry.data[CONF_HOST]).rstrip('/')}/boards/"
-                f"{board.id}"
             ),
         )
